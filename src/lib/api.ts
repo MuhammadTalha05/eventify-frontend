bc63cd6f0dd6b4d28b43e4218f0355570feb6c93
Muhammad Talha <talharashid229@gmail.com>
2025-09-01 22:00:22 +0500

    Changes in Featch User

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -9,28 +9,6 @@ export const API = axios.create({
   },
 });
 
-// // Interceptor for 401 responses
-// API.interceptors.response.use(
-//   (response) => response,
-//   async (error: AxiosError) => {
-//     const originalRequest = error.config as AxiosRequestConfig & { _retry?: boolean };
-
-//     const { refreshAccessToken, user } = useAuthStore.getState();
-
-//     if (error.response?.status === 401 && !originalRequest._retry && user) {
-//       originalRequest._retry = true;
-//       try {
-//         await refreshAccessToken(); // refresh token using cookies
-//         return API(originalRequest); // retry original request
-//       } catch (err) {
-//         // if refresh fails, user will be logged out automatically
-//         return Promise.reject(err);
-//       }
-//     }
-
-//     return Promise.reject(error);
-//   }
-// );
 API.interceptors.response.use(
   (response) => response,
   async (error: AxiosError) => {
@@ -53,26 +31,3 @@ API.interceptors.response.use(
     return Promise.reject(error);
   }
 );
-
-// Interceptor to handle 401
-// API.interceptors.response.use(
-//   (response) => response,
-//   async (error: AxiosError) => {
-//     const originalRequest = error.config as AxiosRequestConfig & { _retry?: boolean };
-
-//     const authStore = useAuthStore.getState();
-
-//     if (error.response?.status === 401 && !originalRequest._retry && authStore.user) {
-//       originalRequest._retry = true;
-//       try {
-//         await authStore.refreshAccessToken(); // Only refresh if user exists
-//         return API(originalRequest); // Retry original request
-//       } catch (err) {
-//         authStore.clearAuth();
-//         return Promise.reject(err);
-//       }
-//     }
-
-//     return Promise.reject(error);
-//   }
-// );
